338631a978e56e1f1b275419d5c83da3e95a1781
Dante <duanjl.china@gmail.com>
2024-10-14 15:41:20 +0800

    test: fix

--- apps/core/scripts/before-test.js
+++ apps/core/scripts/before-test.js
@@ -3,7 +3,7 @@ const path = require('node:path')
 
 const oldPackage = fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8');
 
-const str = oldPackage.replace('{'. `{"type": "module",`);
+const str = oldPackage.replace('{'. '{\n\t"type": "module",');
 
 
 fs.readFileSync(path.join(__dirname, '../package.json'));
